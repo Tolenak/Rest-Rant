b864dad23950408febc8074ae5c632a7d1786c7f
tolenak <tylertyler1009@gmail.com>
2023-10-16 16:54:07 -0700

    Adding styles to Rest-rant

--- Rest Rant Part 1 /index.js
+++ Rest Rant Part 1 /index.js
@@ -1,24 +1,28 @@
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-// requiring express & dotenv
-require('dotenv').config ()
-const express = require ('express')
-const app = express ()
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-app.set('view engine','jsx')
-app.engine ('jsx',require('express-react-views').createengine())
-
-app.use('/places',require('./controllers/places'))
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.sender('home')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
-// pulling PORT variable from the .env file 
+// Listen for Connections
 app.listen(process.env.PORT)
+
 
 
 
