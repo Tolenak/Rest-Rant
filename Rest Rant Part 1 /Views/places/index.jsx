e1a7884228e7505f9f20c69ac6632f7e6e6a8a92
tolenak <tylertyler1009@gmail.com>
2023-10-16 17:08:23 -0700

    adding and rendering

--- Rest Rant Part 1 /Views/places/index.jsx
+++ Rest Rant Part 1 /Views/places/index.jsx
@@ -5,21 +5,24 @@ const Def = require('./default')
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
+        <div className="col-sm-6">
           <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
+          <img src={place.pic} alt={place.name} />
         </div>
       )
     })
     return (
       <Def>
           <main>
-              <h1>PLACES INDEX PAGE</h1>
-              {placesFormatted}
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
+                {placesFormatted}
+              </div>
           </main>
       </Def>
-  )
+    )
   }
+  
   
 function places () {
 
