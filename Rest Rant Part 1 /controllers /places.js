3883092449149fdbc339c27333e9eb43532bafbc
tolenak <tylertyler1009@gmail.com>
2023-10-16 17:27:07 -0700

    New places can be added

--- Rest Rant Part 1 /controllers /places.js
+++ Rest Rant Part 1 /controllers /places.js
@@ -1,6 +1,45 @@
 const router = require('express').Router()
+const places = require ('../models/places.js')
 
-router.get('/', (req, res)=>{
-    res.send('GET /places')
+app.get('/', (req, res)=>{
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/h-thai-ml-tables.png'
+      }, {
+          name: 'Coding Cat Cafe',
+          city: 'Phoenix',
+          state: 'AZ',
+          cuisines: 'Coffee, Bakery',
+          pic: '/images/coffee-cat.jpg'
+      }]
+      
+      
+    res.render('places/index', { places })
 })
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
+  
+  
 module.exports = router
