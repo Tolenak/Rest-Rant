21393f6f10feeb5580c4f79c463637cfac9931ad
tolenak <tylertyler1009@gmail.com>
2023-10-26 15:20:47 -0700

    Implementing comment form to places controller

--- Rest Rant Part 1 /controllers /places.js
+++ Rest Rant Part 1 /controllers /places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { populate } = require('dotenv')
 const db = require('../models')
 
 router.get('/', (req, res) => {
@@ -30,7 +31,9 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  ,populate ('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -59,5 +62,33 @@ router.post('/:id/rant', (req, res) => {
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
 })
+
+router.post('/:id/comment', (req, res)=> {
+  console.log(req.body)
+  req.body.rant = req.body.rant ? true : false 
+  res.send('GET /places/:id/comment stub')
+})
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
 
 module.exports = router
